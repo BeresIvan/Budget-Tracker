8f0c6bc69c6a7a6e11f4443705402f50ed90f67c
GTIvan <ivberes@gmail.com>
2021-02-21 15:21:37 -0600

    opdated the server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
+
 const HOST = process.env.HOST;
 const DB = process.env.DB;
 
@@ -17,7 +20,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://${proccess.env.HOST}/${process.env.DB_NAME}`, {
   useNewUrlParser: true,
   useUnifiedTopoligy: true,
   useCreateIndex: true,
@@ -28,5 +31,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}`);
 });
